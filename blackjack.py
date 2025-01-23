d95f1ff6faa3c0513eb59ac18405cbd384999953
TrentonEnsing <ensingtrent2@gmail.com>
2025-01-23 15:40:50 -0500

    Update blackjack.py
    Added a Deck class to make a deck of cards that can be shuffled and dealt.

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,6 @@
 import random
 
+#CARD CLASS
 class Card:
     def __init__(self, suit, value):
         self.suit = suit
@@ -8,7 +9,23 @@ class Card:
     def __str__(self):
         return(f"{self.value} of {self.suit}")
 
+#DECK CLASS
+class Deck:
+    def __init__(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+        self.cards = [] 
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(suit, value))
 
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self):
+        return self.cards.pop()
+
+#HAND CLASS
 
 
 
